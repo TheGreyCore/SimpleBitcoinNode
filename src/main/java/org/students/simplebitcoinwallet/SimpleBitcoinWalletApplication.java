38b564bfd0e8ad61ba447627eaee34be2337c4f3
Dmitri Matetski <dmitri.matetski@gmail.com>
2024-03-23 17:08:59 +0200

    Added springboot project

---
+++ src/main/java/org/students/simplebitcoinwallet/SimpleBitcoinWalletApplication.java
@@ -0,0 +1,13 @@
+package org.students.simplebitcoinwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBitcoinWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBitcoinWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/students/simplebitcoinwallet/SimpleBitcoinWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package org.students.simplebitcoinwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBitcoinWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
