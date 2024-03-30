e03eaecffbed9e5d975031e0e4f4729328cc3fd4
inugami-dev64 <karl@karlott.net>
2024-03-30 20:32:58 +0200

    Actually add the data models

---
+++ src/main/java/org/students/simplebitcoinwallet/entities/TransactionOutput.java
@@ -0,0 +1,36 @@
+package org.students.simplebitcoinwallet.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "transaction_output")
+public class TransactionOutput implements Serializable {
+    @Id
+    @Getter
+    private Integer id;
+
+    @Getter
+    @Setter
+    @Column(length = 128)
+    @NotNull
+    private String signature;
+
+    @Getter
+    @Setter
+    private BigDecimal amount;
+
+    @Getter
+    @Setter
+    private String receiverPublicKey;
+}
