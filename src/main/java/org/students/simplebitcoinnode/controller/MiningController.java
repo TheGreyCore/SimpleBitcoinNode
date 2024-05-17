7d0fd45767fee10ad494c5f1b8f002d95ef45c7a
Dmitri Matetski <dmitri.matetski@gmail.com>
2024-05-17 23:57:34 +0300

    Created DTO and Endpoint

--- src/main/java/org/students/simplebitcoinnode/controller/MiningController.java
+++ src/main/java/org/students/simplebitcoinnode/controller/MiningController.java
@@ -1,36 +1,18 @@
 package org.students.simplebitcoinnode.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.students.simplebitcoinnode.dto.PoolMiningProposalDTO;
-import org.students.simplebitcoinnode.service.MiningService;
+import org.springframework.web.bind.annotation.*;
+import org.students.simplebitcoinnode.dto.DistributedMiningRequestDTO;
 
 @RestController
-@RequestMapping("/blockchain/mine/")
+@RequestMapping("/blockchain/")
 public class MiningController {
 
-    private final MiningService miningService;
 
-    public MiningController(MiningService miningService) {
-        this.miningService = miningService;
-    }
 
-    /**
-     * Endpoint for proposing a new mining operation.
-     *
-     * @param proposalDTO The data transfer object containing the details of the proposal.
-     * @return ResponseEntity with a message indicating the result of the operation.
-     */
-    @PostMapping("/propose")
-    public ResponseEntity<?> propose (@RequestBody PoolMiningProposalDTO proposalDTO){
-        try {
-            miningService.propose(proposalDTO);
-        } catch (IllegalArgumentException illegalArgumentException){
-            return ResponseEntity.badRequest().body(illegalArgumentException);
-        }
-        return ResponseEntity.ok().body("New propose saved.");
+    @PostMapping("/mine")
+    public ResponseEntity<?> mine (@RequestBody DistributedMiningRequestDTO miningRequestDTO, HttpServletRequest httpServletRequest){
+        return null;
     }
 }
