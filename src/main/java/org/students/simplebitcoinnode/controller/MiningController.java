fec79fd8d42408f4ea0a3a760579ecf268267c5c
Dmitri Matetski <dmitri.matetski@gmail.com>
2024-05-20 01:32:31 +0300

    /propose endpoint and service added.

--- src/main/java/org/students/simplebitcoinnode/controller/MiningController.java
+++ src/main/java/org/students/simplebitcoinnode/controller/MiningController.java
@@ -1,18 +1,36 @@
 package org.students.simplebitcoinnode.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.students.simplebitcoinnode.dto.DistributedMiningRequestDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.students.simplebitcoinnode.dto.PoolMiningProposalDTO;
+import org.students.simplebitcoinnode.service.MiningService;
 
 @RestController
-@RequestMapping("/blockchain/")
+@RequestMapping("/blockchain/mine/")
 public class MiningController {
 
+    private final MiningService miningService;
 
+    public MiningController(MiningService miningService) {
+        this.miningService = miningService;
+    }
 
-    @PostMapping("/mine")
-    public ResponseEntity<?> mine (@RequestBody DistributedMiningRequestDTO miningRequestDTO, HttpServletRequest httpServletRequest){
-        return null;
+    /**
+     * Endpoint for proposing a new mining operation.
+     *
+     * @param proposalDTO The data transfer object containing the details of the proposal.
+     * @return ResponseEntity with a message indicating the result of the operation.
+     */
+    @PostMapping("/propose")
+    public ResponseEntity<?> propose (@RequestBody PoolMiningProposalDTO proposalDTO){
+        try {
+            miningService.propose(proposalDTO);
+        } catch (IllegalArgumentException illegalArgumentException){
+            return ResponseEntity.badRequest().body(illegalArgumentException);
+        }
+        return ResponseEntity.ok().body("New propose saved.");
     }
 }
